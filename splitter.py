51ee156a3a3350cb8c675391bf5637a93aa92dd5
Andrei <fixx@WorkPro.local>
2020-07-17 16:49:01 +0300

    multiple shuffle while creating sub-segments

--- splitter.py
+++ splitter.py
@@ -15,6 +15,7 @@ pd.options.display.float_format = "{:.8f}".format
 
 
 def add_synth_test_col(tmp):
+    print(tmp.head(10))
     if 'ltv' in tmp.columns:
         for i in ['ltv', 'since_rent', 'age']:
             x = tmp[[i]].values.astype(float)  # returns a numpy array
@@ -24,16 +25,16 @@ def add_synth_test_col(tmp):
             tmp[col] = x_scaled.astype(float)
             tmp.drop(columns=[i], inplace=True)
     else:
-        for i in ['age']:
+        print('Here')
+        for i in ['age', 'dsa']:
             x = tmp[[i]].values.astype(float)  # returns a numpy array
             min_max_scaler = preprocessing.MinMaxScaler()
             x_scaled = min_max_scaler.fit_transform(x)
             col = i + '_norm'
             tmp[col] = x_scaled.astype(float)
-            tmp.drop(columns=['dsa', 'age'], inplace=True)
+            tmp.drop(columns=[i], inplace=True)
     tmp['homo_t'] = tmp[list(tmp.columns)].sum(axis=1)
     df = tmp[['homo_t']]
-    print(df['homo_t'].describe())
     return df
 
 
@@ -46,20 +47,23 @@ def add_synth_test_col(tmp):
 def shuffle_test(tmp, v):
     n = 1
     while True:
-        print(f"Shuffle number {n}")
-        shuffled = tmp.sample(frac=1, replace=False, weights=tmp.groupby(
-            'homo_t')['homo_t'].transform('count'))
-        result = np.array_split(shuffled, v)
+        print(f"Iteration number {n}")
+        rws = len(tmp)//v
+        result = []
+        for i in range(v-1):
+            i = tmp.sample(n=rws, replace=False)
+            tmp.drop(i.index, inplace=True)
+            result.append(i)
+        result.append(tmp)
         test_arr = [x['homo_t'].values for x in result]
         stat, p = levene(*test_arr)
         pyval = p.item()
-        print('p-value due to Levene test:')
-        print('{0:40f}'.format(pyval))
+        print('p-value due to Levene test:', '{0:4f}'.format(pyval))
         n += 1
         if pyval > 0.05:
             return result
             break
-        elif n > 5:
+        elif n > 500:
             break
 
 
@@ -78,20 +82,22 @@ def split_save(df, chunks):
         tmp = add_synth_test_col(tmp)
         print(f"Sampling {v} chunks in {k}")
         result = shuffle_test(tmp, v)
-        print(f"All {v} chunks in {k} are in equal variance according to \
-Levene test")
-        print('Saving chunks...')
-        names = [k + '_' + str(n) + '.csv' for n in range(1, v+1)]
-        for part, name in zip(result, names):
-            part.drop(columns=['homo_t'], inplace=True)
-            outdir = './segments'
-            if not os.path.exists(outdir):
-                os.mkdir(outdir)
-            part.to_csv(os.path.join(outdir, name), encoding='utf-8')
+        if result:
+            print(f"All {v} chunks in {k} are in equal variance according to \
+    Levene test")
+            print('Saving chunks...')
+            names = [k + '_' + str(n) + '.csv' for n in range(1, v+1)]
+            for part, name in zip(result, names):
+                part.drop(columns=['homo_t'], inplace=True)
+                outdir = './segments'
+                if not os.path.exists(outdir):
+                    os.mkdir(outdir)
+                part.to_csv(os.path.join(outdir, name), encoding='utf-8')
+        else:
+            print('Stop shuffling...')
 
 
 if __name__ == "__main__":
-
     df = gr.get_riders_seg()
     chunks = {'SEG1': 2, 'SEG2': 8, 'SEG3': 2, 'SEG4': 2, 'SEG5': 10,
               'SEG6': 8, 'SEG7': 2, 'SEG8': 2, 'SEG9': 2}
